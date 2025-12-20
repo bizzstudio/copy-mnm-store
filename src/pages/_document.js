e8c3248e76439c19814c4716f67cf40c6f86cef0
BizzStudio <office@bizzstudio.co.il>
2025-12-20 19:03:28 +0200

    Remove Google Tag Manager script from _document.js
    Removed Google Tag Manager script from the document head.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -136,22 +136,6 @@ class MyDocument extends Document {
           {/* עדכון הקישור ל-manifest */}
           <link rel="manifest" href="/api/manifest" />
 
- {/* --- Google Tag Manager SCRIPT בתוך ה-HEAD --- */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),
-                    dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5CFPG5F');`,
-            }}
-          />
-
         </Head>
         <body>
 {/* ----- BODY START SCRIPTS ----- */}
@@ -159,15 +143,7 @@ class MyDocument extends Document {
             <script dangerouslySetInnerHTML={{ __html: bodyStart }} />
           )}
 
-          {/* ----- NOSCRIPT (GTM) – ללא עטיפה בכלל ----- */}
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `
-                <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5CFPG5F"
-                height="0" width="0" style="display:none;visibility:hidden"></iframe>
-              `
-            }}
-          />
+
           <Main />
           <NextScript />
 
